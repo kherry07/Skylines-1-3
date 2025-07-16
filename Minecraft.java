0239401fc71914a02f7ffdfb7bee09df8c87c7ed
Kherry <kherryale07@gmail.com>
2025-07-16 07:26:16 -0600

    segundo git desde la computadora

--- Minecraft.java
+++ Minecraft.java
@@ -5,23 +5,13 @@ public class Minecraft {
 	public static void main(String[] args) {
 		// TODO Auto-generated method st
 		        Scanner scanner = new Scanner(System.in);
-
-		        
-		        System.out.print("Ingresa una frase: ");
+		        System.out.print("Enter a phrase: ");
 		        String frase = scanner.nextLine();
-
-		       
-		        String cadenaInvertida = "";
-
-		        
+		        String cadenaInvertida = "";    
 		        for (int i = frase.length() - 1; i >= 0; i--) {
 		            cadenaInvertida += frase.charAt(i); 
 		        }
-
-		      
-		        System.out.println("Frase invertida: " + cadenaInvertida);
-
-		        
+		        System.out.println("inverted phrase: " + cadenaInvertida);
 		    }
 		
 
